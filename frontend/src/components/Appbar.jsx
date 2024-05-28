72e0fd618a864e5b78f56bc799876fc6dec8a777
Koushubh <69477084+koushub@users.noreply.github.com>
2024-05-28 17:00:53 +0530

    1

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -10,7 +10,7 @@ export const Appbar = () => {
 
     return <div className="shadow h-14 flex justify-between">
         <div className="flex flex-col justify-center h-full ml-4">
-            PayTM App
+            Login App
         </div>
         <div className="flex">
             <div className="flex flex-col justify-center h-full mr-4">
