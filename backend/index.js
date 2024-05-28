1993ba8c0c2a46512973b855d23e52bc0f1c73b3
Koushubh <69477084+koushub@users.noreply.github.com>
2024-05-28 13:21:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-//2. Add body-parser
-//Since we have to support the JSON body in post requests, 
-//add the express body parser middleware to backend/index.js
-//You can use the body-parser npm library, or use express.json 
+
 app.use(express.json());
 app.use("/api/v1", rootRouter);
 app.listen(3000, () => {
